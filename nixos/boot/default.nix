34bb039b6f8c1f1b5d369482e1cdc2f902c106bd
Ilham Putra Husada <hambosto@gmail.com>
2025-04-29 18:38:28 +0700

    feat(systemd): disable coredump completely

--- nixos/boot/default.nix
+++ nixos/boot/default.nix
@@ -9,6 +9,10 @@
       efi.canTouchEfiVariables = true;
       systemd-boot.enable = true;
     };
+    kernel.sysctl = {
+      "kernel.core_pattern" = "|/bin/false";
+      "fs.suid_dumpable" = 0;
+    };
     tmp.cleanOnBoot = true;
   };
 }
--- nixos/security/default.nix
+++ nixos/security/default.nix
@@ -1,4 +1,18 @@
 {
   security.rtkit.enable = true;
   security.polkit.enable = true;
+  security.pam.loginLimits = [
+    {
+      domain = "*";
+      type = "hard";
+      item = "core";
+      value = "0";
+    }
+    {
+      domain = "*";
+      type = "soft";
+      item = "core";
+      value = "0";
+    }
+  ];
 }
