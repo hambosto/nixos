e08511e8b36b30f17a6a2fe73c45eff8771207b4
Ilham Putra Husada <hambosto@gmail.com>
2025-05-08 20:14:40 +0700

    refactor(home-manager): refactor screenshot script for readability

--- home-manager/scripts/screenshot.nix
+++ home-manager/scripts/screenshot.nix
@@ -8,28 +8,23 @@ let
   screenshot = pkgs.writeShellScriptBin "screenshot" ''
     SAVE_DIR="${config.home.homeDirectory}/Pictures/Screenshots"
     TIMESTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
-
     mkdir -p "$SAVE_DIR"
-
-    take_screenshot() {
-      local mode=$1
-      local filename="$SAVE_DIR/screenshot_$TIMESTAMP.png"
-      
-      case "$mode" in
-        selection) target_mode="area" ;;
-        everything) target_mode="screen" ;;
-        *) echo "Invalid mode: $mode" && exit 1 ;;
-      esac
-      
-      ${lib.getExe pkgs.grimblast} --notify save "$target_mode" "$filename"
-    }
 
     if [ $# -eq 0 ]; then
       echo "Usage: screenshot [selection|everything]"
       exit 1
     fi
 
-    take_screenshot "$1"
+    mode=$1
+    filename="$SAVE_DIR/screenshot_$TIMESTAMP.png"
+
+    case "$mode" in
+      selection) target_mode="area" ;;
+      everything) target_mode="screen" ;;
+      *) echo "Invalid mode: $mode" && exit 1 ;;
+    esac
+
+    ${lib.getExe pkgs.grimblast} --notify save "$target_mode" "$filename"
   '';
 in
 {
