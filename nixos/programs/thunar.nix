014a8feb778241445987ca5ee8aa29b14b2fc0ff
Ilham Putra Husada <hambosto@gmail.com>
2025-05-13 02:02:02 +0700

    feat(thunar): set kitty as default terminal emulator

--- nixos/programs/thunar.nix
+++ nixos/programs/thunar.nix
@@ -1,13 +1,23 @@
 {
+  config,
+  lib,
   pkgs,
+  username,
   ...
 }:
 {
-  programs.thunar = {
-    enable = true;
-    plugins = with pkgs.xfce; [
-      thunar-archive-plugin
-      thunar-volman
-    ];
+  config = {
+    programs.thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-volman
+      ];
+    };
+
+    home-manager.users.${username}.xdg.configFile."xfce4/helpers.rc".text =
+      lib.mkIf config.programs.thunar.enable ''
+        TerminalEmulator=kitty
+      '';
   };
 }
