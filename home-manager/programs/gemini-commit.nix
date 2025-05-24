53ba034e3900b60cfc9ed9838948dc8f7b616a47
Ilham Putra Husada <hambosto@gmail.com>
2025-05-24 18:48:22 +0700

    chore: update gemini-commit to 0.4.0
    Updates gemini-commit to version 0.4.0. This includes updating
    the source and vendor hashes.

--- home-manager/programs/gemini-commit.nix
+++ home-manager/programs/gemini-commit.nix
@@ -11,12 +11,12 @@ let
       cfg = config.programs.gemini-commit;
       gemini-commit-module = pkgs.buildGoModule {
         pname = "geminicommit";
-        version = "0.3.2";
+        version = "0.4.0";
         src = pkgs.fetchFromGitHub {
           owner = "tfkhdyt";
           repo = "geminicommit";
-          rev = "b58460fc7c7d3a14caf25826350a69757afa0857";
-          hash = "sha256-GOVLuLu7o66BvyGvja3dMfdgS/N83yFhPWsptzgzhws=";
+          rev = "414112c4bcffebd1c93688de80f0a9d7cdfa0cb9";
+          hash = "sha256-fwcS3+lbUGJ6BK43HHAzO2sM9JQ1sNA+xXHYa02EbJQ=";
         };
         vendorHash = "sha256-IfqlPg+HPcOfjlwwuLi2/R21UD83xQzWyUmzM7JSDEs=";
 
