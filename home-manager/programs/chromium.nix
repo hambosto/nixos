86908ea2ac8cad532d785d176b6239baaf9658a3
Ilham Putra Husada <hambosto@gmail.com>
2025-05-30 02:11:24 +0700

    chore: disable chromium and flake registry
    Disables chromium in home-manager and flake registry in nixos.

--- home-manager/programs/chromium.nix
+++ home-manager/programs/chromium.nix
@@ -1,31 +1,17 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-{
-  programs.chromium = {
+  programs.brave = {
     enable = true;
-    package = lib.mkForce (
-      pkgs.chromium.override {
-        commandLineArgs = lib.concatStringsSep " " config.programs.chromium.commandLineArgs;
-      }
-    );
     commandLineArgs = [
       "--ozone-platform=wayland"
       "--ozone-platform-hint=auto"
-      "--enable-features=WebRTCPipeWireCapturer"
-      "--ignore-gpu-blocklist"
-      "--enable-gpu-rasterization"
     ];
     extensions = [
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
+      # { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
       { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock
       { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
       # { id = "bfnaelmomeimhlpmgjnjophhpkkoljpa"; } # Phantom Wallet
       # { id = "nkbihfbeogaeaoehlefnkodbefgpgknn"; } # MetaMask
-      { id = "bggfcpfjbdkhfhfmkjpbhnkhmpgjnjph"; } # Material Icons for GitHub
+      { id = "bggfcpfjbdkhfhfmkjpbhnkhnpjjeomc"; } # Material Icon for GitHub
     ];
   };
 }
