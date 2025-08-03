01fd686091a5d8c9bfda7fe1162fc3c28706ed4a
hambosto <106536348+hambosto@users.noreply.github.com>
2025-08-04 00:37:46 +0700

    feat: replace swayidle with hypridle
    This commit replaces swayidle with hypridle for better
    integration with Hyprland. It also removes the swayidle
    service file.

---
+++ home-manager/services/hypridle.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+{
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "${lib.getExe pkgs.swaylock-effects}";
+        before_sleep_cmd = "${lib.getExe' pkgs.systemd "loginctl"} lock-session";
+        after_sleep_cmd = "${lib.getExe' pkgs.hyprland "hyprctl"} dispatch dpms on";
+      };
+      listener = [
+        {
+          timeout = 120;
+          on-timeout = "${lib.getExe pkgs.brightnessctl} -s set 10";
+          on-resume = "${lib.getExe pkgs.brightnessctl} -r";
+        }
+        {
+          timeout = 120;
+          on-timeout = "${lib.getExe pkgs.brightnessctl} -sd asus::kbd_backlight set 0";
+          on-resume = "${lib.getExe pkgs.brightnessctl} -rd asus::kbd_backlight";
+        }
+        {
+          timeout = 180;
+          on-timeout = "${lib.getExe' pkgs.systemd "loginctl"} lock-session";
+        }
+        {
+          timeout = 240;
+          on-timeout = "${lib.getExe' pkgs.hyprland "hyprctl"} dispatch dpms off";
+          on-resume = "${lib.getExe' pkgs.hyprland "hyprctl"} dispatch dpms on && ${lib.getExe pkgs.brightnessctl} -r";
+        }
+        {
+          timeout = 300;
+          on-timeout = "${lib.getExe' pkgs.systemd "systemctl"} suspend";
+        }
+      ];
+    };
+  };
+}
