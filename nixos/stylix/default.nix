0e966f738524f5e4129cb991107507145a3bface
hambosto <106536348+hambosto@users.noreply.github.com>
2025-08-15 00:24:10 +0700

    chore(stylix): update default wallpaper image

--- nixos/stylix/default.nix
+++ nixos/stylix/default.nix
@@ -67,7 +67,7 @@
     #   sha256 = "sha256-qVsCSAlUB+8GLDmlfSEOZ5/FNoFIPP04BhBCOkQyKkg=";
     # };
 
-    image = ../../assets/nixos_wp_cat_glow.png;
+    image = ../../assets/universe.png;
 
     polarity = "dark";
 
