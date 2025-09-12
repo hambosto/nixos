f3385bf4827bee88bf89c5bddd47e43c9e453812
hambosto <106536348+hambosto@users.noreply.github.com>
2025-09-12 14:34:48 +0700

    style(stylix): switch cursor theme to macOS
    Updates the cursor package from Bibata-Modern-Ice to macOS.
    Also updates flake inputs for various dependencies.

--- nixos/stylix/default.nix
+++ nixos/stylix/default.nix
@@ -31,8 +31,8 @@
     polarity = "dark";
 
     cursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
+      package = pkgs.apple-cursor;
+      name = "macOS";
       size = 24;
     };
 
