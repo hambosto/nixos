da9a213956c0ed9436e8f7564347caaa6d9a098e
hambosto <106536348+hambosto@users.noreply.github.com>
2025-09-26 19:06:16 +0700

    Revert "feat(boot): add mac-style plymouth theme"
    This reverts commit 959b0924a2a662db2c5d90a0a6479739aebaa9fb.

--- flake.nix
+++ flake.nix
@@ -26,10 +26,6 @@
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    mac-style-plymouth = {
-      url = "github:SergioRibera/s4rchiso-plymouth-theme";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
