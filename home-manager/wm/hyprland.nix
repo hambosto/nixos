1545a6125333a932f9864ef137239d5d920094ac
hambosto <106536348+hambosto@users.noreply.github.com>
2025-10-04 19:14:13 +0700

    refactor(hyprland): reorder config sections
    Reordered various Hyprland configuration blocks for improved readability and
    organization.
    
    Updated flake.lock to the latest revisions.

--- home-manager/wm/hyprland.nix
+++ home-manager/wm/hyprland.nix
@@ -7,70 +7,138 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    xwayland.enable = true;
     systemd.enable = true;
-    settings = {
-      monitor = [ ", preferred, auto, 1" ];
-      env = [
-        "XDG_CURRENT_DESKTOP,Hyprland"
-        "XDG_SESSION_TYPE,wayland"
-        "XDG_SESSION_DESKTOP,Hyprland"
-        "QT_QPA_PLATFORM,wayland;xcb"
-        "QT_QPA_PLATFORMTHEME,qt6ct"
-        "QT_QPA_PLATFORMTHEME,qt5ct"
-        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-        "QT_AUTO_SCREEN_SCALE_FACTOR,1"
-        "GDK_SCALE,1"
-        "GDK_BACKEND,wayland,x11,*"
-        "CLUTTER_BACKEND,wayland"
-        "MOZ_ENABLE_WAYLAND,1"
-        "APPIMAGELAUNCHER_DISABLE,1"
-        "OZONE_PLATFORM,wayland"
-        "ELECTRON_OZONE_PLATFORM_HINT,wayland"
-      ];
+    xwayland.enable = true;
 
-      general = {
-        gaps_in = 6;
-        gaps_out = 8;
-        border_size = 2;
-        resize_on_border = true;
-        layout = "dwindle";
-      };
+    settings = {
 
       animations = {
         enabled = true;
-        bezier = [
-          "linear, 0, 0, 1, 1"
-          "md3_standard, 0.2, 0, 0, 1"
-          "md3_decel, 0.05, 0.7, 0.1, 1"
-          "md3_accel, 0.3, 0, 0.8, 0.15"
-          "overshot, 0.05, 0.9, 0.1, 1.1"
-          "crazyshot, 0.1, 1.5, 0.76, 0.92"
-          "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
-          "menu_decel, 0.1, 1, 0, 1"
-          "menu_accel, 0.38, 0.04, 1, 0.07"
-          "easeInOutCirc, 0.85, 0, 0.15, 1"
-          "easeOutCirc, 0, 0.55, 0.45, 1"
-          "easeOutExpo, 0.16, 1, 0.3, 1"
-          "softAcDecel, 0.26, 0.26, 0.15, 1"
-          "md2, 0.4, 0, 0.2, 1"
-        ];
         animation = [
+          "border, 1, 10, default"
+          "fade, 1, 3, md3_decel"
+          "fadeLayersIn, 1, 2, menu_decel"
+          "fadeLayersOut, 1, 4.5, menu_accel"
+          "layersIn, 1, 3, menu_decel, slide"
+          "layersOut, 1, 1.6, menu_accel"
+          "specialWorkspace, 1, 3, md3_decel, slidevert"
           "windows, 1, 3, md3_decel, popin 60%"
           "windowsIn, 1, 3, md3_decel, popin 60%"
           "windowsOut, 1, 3, md3_accel, popin 60%"
-          "border, 1, 10, default"
-          "fade, 1, 3, md3_decel"
-          "layersIn, 1, 3, menu_decel, slide"
-          "layersOut, 1, 1.6, menu_accel"
-          "fadeLayersIn, 1, 2, menu_decel"
-          "fadeLayersOut, 1, 4.5, menu_accel"
           "workspaces, 1, 7, menu_decel, slide"
-          "specialWorkspace, 1, 3, md3_decel, slidevert"
+        ];
+
+        bezier = [
+          "crazyshot, 0.1, 1.5, 0.76, 0.92"
+          "easeInOutCirc, 0.85, 0, 0.15, 1"
+          "easeOutCirc, 0, 0.55, 0.45, 1"
+          "easeOutExpo, 0.16, 1, 0.3, 1"
+          "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
+          "linear, 0, 0, 1, 1"
+          "md2, 0.4, 0, 0.2, 1"
+          "md3_accel, 0.3, 0, 0.8, 0.15"
+          "md3_decel, 0.05, 0.7, 0.1, 1"
+          "md3_standard, 0.2, 0, 0, 1"
+          "menu_accel, 0.38, 0.04, 1, 0.07"
+          "menu_decel, 0.1, 1, 0, 1"
+          "overshot, 0.05, 0.9, 0.1, 1.1"
+          "softAcDecel, 0.26, 0.26, 0.15, 1"
         ];
       };
 
+      bind = [
+        # Applications
+        "SUPER, B, exec, ${lib.getExe config.programs.firefox.package}"
+        "SUPER, E, exec, ${lib.getExe config.programs.kitty.package} -e ${lib.getExe config.programs.yazi.package}"
+        "SUPER, L, exec, ${lib.getExe config.programs.hyprlock.package}"
+        "SUPER, PRINT, exec, screenshot screen"
+        "SUPER, RETURN, exec, ${lib.getExe config.programs.kitty.package} --title Terminal"
+        "SUPER, SPACE, exec, ${lib.getExe config.programs.rofi.package} -show drun"
+        "SUPER SHIFT, M, exec, ${lib.getExe config.programs.kitty.package} -e ${lib.getExe config.programs.htop.package}"
+        "SUPER SHIFT, PRINT, exec, screenshot area"
+
+        # Window Management
+        "SUPER, down, movefocus, d"
+        "SUPER, F, fullscreen, 0"
+        "SUPER, G, togglegroup"
+        "SUPER, J, togglesplit"
+        "SUPER, K, swapsplit"
+        "SUPER, left, movefocus, l"
+        "SUPER, M, fullscreen, 1"
+        "SUPER, Q, killactive"
+        "SUPER, right, movefocus, r"
+        "SUPER, T, togglefloating"
+        "SUPER, up, movefocus, u"
+        "SUPER ALT, down, swapwindow, d"
+        "SUPER ALT, left, swapwindow, l"
+        "SUPER ALT, right, swapwindow, r"
+        "SUPER ALT, up, swapwindow, u"
+        "SUPER SHIFT, down, resizeactive, 0 100"
+        "SUPER SHIFT, left, resizeactive, -100 0"
+        "SUPER SHIFT, Q, exec, hyprctl activewindow | grep pid | tr -d 'pid:' | xargs kill"
+        "SUPER SHIFT, right, resizeactive, 100 0"
+        "SUPER SHIFT, T, workspaceopt, allfloat"
+        "SUPER SHIFT, up, resizeactive, 0 -100"
+
+        # Workspaces - Switch
+        "SUPER, 0, workspace, 10"
+        "SUPER, 1, workspace, 1"
+        "SUPER, 2, workspace, 2"
+        "SUPER, 3, workspace, 3"
+        "SUPER, 4, workspace, 4"
+        "SUPER, 5, workspace, 5"
+        "SUPER, 6, workspace, 6"
+        "SUPER, 7, workspace, 7"
+        "SUPER, 8, workspace, 8"
+        "SUPER, 9, workspace, 9"
+        "SUPER, mouse_down, workspace, e+1"
+        "SUPER, mouse_up, workspace, e-1"
+        "SUPER, Tab, workspace, m+1"
+        "SUPER CTRL, down, workspace, empty"
+        "SUPER SHIFT, Tab, workspace, m-1"
+
+        # Workspaces - Move Window
+        "SUPER SHIFT, 0, movetoworkspace, 10"
+        "SUPER SHIFT, 1, movetoworkspace, 1"
+        "SUPER SHIFT, 2, movetoworkspace, 2"
+        "SUPER SHIFT, 3, movetoworkspace, 3"
+        "SUPER SHIFT, 4, movetoworkspace, 4"
+        "SUPER SHIFT, 5, movetoworkspace, 5"
+        "SUPER SHIFT, 6, movetoworkspace, 6"
+        "SUPER SHIFT, 7, movetoworkspace, 7"
+        "SUPER SHIFT, 8, movetoworkspace, 8"
+        "SUPER SHIFT, 9, movetoworkspace, 9"
+
+        # Fn keys
+        ", XF86AudioLowerVolume, exec, ${lib.getExe' pkgs.wireplumber "wpctl"} set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioMicMute, exec, ${lib.getExe' pkgs.wireplumber "wpctl"} set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ", XF86AudioMute, exec, ${lib.getExe' pkgs.wireplumber "wpctl"} set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioRaiseVolume, exec, ${lib.getExe' pkgs.wireplumber "wpctl"} set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1"
+        ", XF86MonBrightnessDown, exec, ${lib.getExe pkgs.brightnessctl} set 5%-"
+        ", XF86MonBrightnessUp, exec, ${lib.getExe pkgs.brightnessctl} set +5%"
+      ];
+
+      binde = [
+        "ALT, Tab, bringactivetotop"
+        "ALT, Tab, cyclenext"
+      ];
+
+      bindm = [
+        "SUPER, mouse:272, movewindow"
+        "SUPER, mouse:273, resizewindow"
+      ];
+
       decoration = {
+        blur = {
+          enabled = true;
+          ignore_opacity = true;
+          new_optimizations = true;
+          passes = 3;
+          size = 9;
+          xray = false;
+        };
+
+        dim_inactive = false;
         rounding = 5;
 
         shadow = {
@@ -79,27 +147,55 @@
           render_power = 3;
           sharp = false;
         };
+      };
 
-        blur = {
-          enabled = true;
-          size = 9;
-          passes = 3;
-          ignore_opacity = true;
-          new_optimizations = true;
-          xray = false;
-        };
+      dwindle = {
+        preserve_split = true;
+        pseudotile = true;
+      };
 
-        dim_inactive = false;
+      ecosystem = {
+        no_update_news = true;
+      };
+
+      env = [
+        "APPIMAGELAUNCHER_DISABLE,1"
+        "CLUTTER_BACKEND,wayland"
+        "ELECTRON_OZONE_PLATFORM_HINT,wayland"
+        "GDK_BACKEND,wayland,x11,*"
+        "GDK_SCALE,1"
+        "MOZ_ENABLE_WAYLAND,1"
+        "OZONE_PLATFORM,wayland"
+        "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+        "QT_QPA_PLATFORM,wayland;xcb"
+        "QT_QPA_PLATFORMTHEME,qt5ct"
+        "QT_QPA_PLATFORMTHEME,qt6ct"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+        "XDG_SESSION_TYPE,wayland"
+      ];
+
+      general = {
+        border_size = 2;
+        gaps_in = 6;
+        gaps_out = 8;
+        layout = "dwindle";
+        resize_on_border = true;
+      };
+
+      gestures = {
+        gesture = "3, horizontal, workspace";
       };
 
       input = {
+        follow_mouse = 1;
         kb_layout = "us";
-        kb_variant = "";
         kb_model = "";
         kb_options = "";
-        numlock_by_default = true;
+        kb_variant = "";
         mouse_refocus = false;
-        follow_mouse = 1;
+        numlock_by_default = true;
         sensitivity = 0;
 
         touchpad = {
@@ -114,124 +210,17 @@
         initial_workspace_tracking = 1;
       };
 
-      xwayland = {
-        force_zero_scaling = true;
-      };
+      monitor = [ ", preferred, auto, 1" ];
 
       windowrulev2 = [
         "float,title:^(Picture-in-Picture)$"
-        "pin,title:^(Picture-in-Picture)$"
         "move 69.5% 4%,title:^(Picture-in-Picture)$"
-
-        "float,class:(.*org.pulseaudio.pavucontrol.*)"
-        "size 700 600,class:(.*org.pulseaudio.pavucontrol.*)"
-        "center,class:(.*org.pulseaudio.pavucontrol.*)"
-        "pin,class:(.*org.pulseaudio.pavucontrol.*)"
-
+        "pin,title:^(Picture-in-Picture)$"
       ];
 
-      gestures = {
-        gesture = "3, horizontal, workspace";
+      xwayland = {
+        force_zero_scaling = true;
       };
-
-      dwindle = {
-        pseudotile = true;
-        preserve_split = true;
-      };
-
-      ecosystem = {
-        no_update_news = true;
-      };
-
-      bind = [
-        "SUPER, RETURN, exec, ${lib.getExe config.programs.kitty.package} --title Terminal"
-        "SUPER, E, exec, ${lib.getExe config.programs.kitty.package} -e ${lib.getExe config.programs.yazi.package}"
-        "SUPER, L, exec, ${lib.getExe config.programs.hyprlock.package}"
-        "SUPER, B, exec, ${lib.getExe config.programs.firefox.package}"
-        "SUPER SHIFT, M, exec, ${lib.getExe config.programs.kitty.package} -e ${lib.getExe config.programs.htop.package}"
-        "SUPER, SPACE, exec, ${lib.getExe config.programs.rofi.package} -show drun"
-        "SUPER, PRINT, exec, screenshot screen"
-        "SUPER SHIFT, PRINT, exec, screenshot area"
-
-        "SUPER, Q, killactive"
-        "SUPER SHIFT, Q, exec, hyprctl activewindow | grep pid | tr -d 'pid:' | xargs kill"
-        "SUPER, F, fullscreen, 0"
-        "SUPER, M, fullscreen, 1"
-        "SUPER, T, togglefloating"
-        "SUPER SHIFT, T, workspaceopt, allfloat"
-        "SUPER, J, togglesplit"
-        "SUPER, left, movefocus, l"
-        "SUPER, right, movefocus, r"
-        "SUPER, up, movefocus, u"
-        "SUPER, down, movefocus, d"
-        "SUPER SHIFT, right, resizeactive, 100 0"
-        "SUPER SHIFT, left, resizeactive, -100 0"
-        "SUPER SHIFT, down, resizeactive, 0 100"
-        "SUPER SHIFT, up, resizeactive, 0 -100"
-        "SUPER, G, togglegroup"
-        "SUPER, K, swapsplit"
-        "SUPER ALT, left, swapwindow, l"
-        "SUPER ALT, right, swapwindow, r"
-        "SUPER ALT, up, swapwindow, u"
-        "SUPER ALT, down, swapwindow, d"
-
-        # Workspaces
-        "SUPER, 1, workspace, 1"
-        "SUPER, 2, workspace, 2"
-        "SUPER, 3, workspace, 3"
-        "SUPER, 4, workspace, 4"
-        "SUPER, 5, workspace, 5"
-        "SUPER, 6, workspace, 6"
-        "SUPER, 7, workspace, 7"
-        "SUPER, 8, workspace, 8"
-        "SUPER, 9, workspace, 9"
-        "SUPER, 0, workspace, 10"
-
-        "SUPER SHIFT, 1, movetoworkspace, 1"
-        "SUPER SHIFT, 2, movetoworkspace, 2"
-        "SUPER SHIFT, 3, movetoworkspace, 3"
-        "SUPER SHIFT, 4, movetoworkspace, 4"
-        "SUPER SHIFT, 5, movetoworkspace, 5"
-        "SUPER SHIFT, 6, movetoworkspace, 6"
-        "SUPER SHIFT, 7, movetoworkspace, 7"
-        "SUPER SHIFT, 8, movetoworkspace, 8"
-        "SUPER SHIFT, 9, movetoworkspace, 9"
-        "SUPER SHIFT, 0, movetoworkspace, 10"
-
-        "SUPER ALT, left, swapwindow, l"
-        "SUPER ALT, right, swapwindow, r"
-        "SUPER ALT, up, swapwindow, u"
-        "SUPER ALT, down, swapwindow, d"
-
-        "SUPER, Tab, workspace, m+1"
-        "SUPER SHIFT, Tab, workspace, m-1"
-
-        "SUPER, mouse_down, workspace, e+1"
-        "SUPER, mouse_up, workspace, e-1"
-        "SUPER CTRL, down, workspace, empty"
-
-        "SUPER, mouse_down, workspace, e+1"
-        "SUPER, mouse_up, workspace, e-1"
-        "SUPER CTRL, down, workspace, empty"
-
-        # Fn keys
-        ", XF86AudioRaiseVolume, exec, ${lib.getExe' pkgs.wireplumber "wpctl"} set-volume @DEFAULT_AUDIO_SINK@ 5%+ -l 1"
-        ", XF86AudioLowerVolume, exec, ${lib.getExe' pkgs.wireplumber "wpctl"} set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-        ", XF86MonBrightnessUp, exec, ${lib.getExe pkgs.brightnessctl} set +5%"
-        ", XF86MonBrightnessDown, exec, ${lib.getExe pkgs.brightnessctl} set 5%-"
-        ", XF86AudioMute, exec, ${lib.getExe' pkgs.wireplumber "wpctl"} set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        ", XF86AudioMicMute, exec, ${lib.getExe' pkgs.wireplumber "wpctl"} set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-      ];
-
-      bindm = [
-        "SUPER, mouse:272, movewindow"
-        "SUPER, mouse:273, resizewindow"
-      ];
-
-      binde = [
-        "ALT, Tab, cyclenext"
-        "ALT, Tab, bringactivetotop"
-      ];
     };
   };
 }
