b730a97120a9df80e33184700d3a3eb1186ff6a1
hambosto <106536348+hambosto@users.noreply.github.com>
2025-10-13 11:21:55 +0700

    feat(qemu): enable ovmf with secure boot and tpm
    Configures QEMU to use OVMF firmware with secure boot and TPM. Updates flake inputs.

--- nixos/virtualisation/qemu.nix
+++ nixos/virtualisation/qemu.nix
@@ -1,19 +1,9 @@
-{ pkgs, ... }:
 {
   virtualisation.libvirtd = {
     enable = false;
     qemu = {
       runAsRoot = true;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (pkgs.OVMFFull.override {
-            secureBoot = true;
-            tpmSupport = true;
-          }).fd
-        ];
-      };
     };
   };
 }
