3eaadf5e2607cec6e26ae919acc16a1551dd1e0d
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2025-11-19 16:41:52 +0700

    Revert "style(desktop): refine visual configuration"
    This reverts commit 36de782ed9d22fc960f04ee9bfd121b259a27609.

--- home-manager/programs/kitty.nix
+++ home-manager/programs/kitty.nix
@@ -7,7 +7,8 @@
       size = 11;
     };
     settings = lib.mkForce {
-      background_opacity = 0.7;
+      background_blur = 10;
+      background_opacity = 0.4;
       bold_font = "auto";
       bold_italic_font = "auto";
       confirm_os_window_close = 0;
