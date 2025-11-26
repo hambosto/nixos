2b3b9d7bcdcc2be7baa7c8f3349716ac91f191ff
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2025-11-26 14:46:26 +0700

    feat(boot): enable lanzaboote for secure boot
    Switches from systemd-boot to lanzaboote for secure boot management.
    Enables systemd in the initrd, likely a requirement for lanzaboote.
    Updates yazi plugins to the latest revision.

--- home-manager/programs/yazi.nix
+++ home-manager/programs/yazi.nix
@@ -3,8 +3,8 @@ let
   yazi-plugins = pkgs.fetchFromGitHub {
     owner = "yazi-rs";
     repo = "plugins";
-    rev = "8f1d9711bcd0e48af1fcb4153c16d24da76e732d";
-    hash = "sha256-7vsqHvdNimH/YVWegfAo7DfJ+InDr3a1aNU0f+gjcdw=";
+    rev = "eaf6920b7439fa7164a1c162a249c3c76dc0acd9";
+    hash = "sha256-B6zhZfp3SEaiviIzosI2aD8fk+hQF0epOTKi1qm8V3E=";
   };
 in
 {
