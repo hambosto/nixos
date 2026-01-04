ccd8ff8539fff9c8c375e88c40c871aab6b3d007
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2026-01-04 20:46:32 +0700

    chore(nixos): disable gnome-keyring
    Explicitly disables gnome-keyring in core services configuration.
    Also refactors dbus service configuration for improved readability.

--- nixos/services/core.nix
+++ nixos/services/core.nix
@@ -1,11 +1,10 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   services = {
-    dbus = {
-      enable = true;
-      packages = [ pkgs.nautilus ];
-    };
+    dbus.enable = true;
+    dbus.packages = [ pkgs.nautilus ];
     fstrim.enable = true;
+    gnome.gnome-keyring.enable = lib.mkForce false;
     gvfs.enable = true;
     power-profiles-daemon.enable = true;
     upower.enable = true;
