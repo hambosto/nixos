c0f39b4e1eed5fc7588b6f0454f4e58a4f15bbb1
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2026-05-10 13:32:50 +0700

    chore: update flake.lock, switch to linuxPackages_latest, refactor coredump config

--- nixos/systemd/coredump.nix
+++ nixos/systemd/coredump.nix
@@ -1,7 +1,11 @@
 {
-  systemd.coredump.enable = false;
-  systemd.coredump.extraConfig = ''
-    Storage=none
-    ProcessSizeMax=0
-  '';
+  systemd.coredump = {
+    enable = false;
+    settings = {
+      Coredump = {
+        Storage = "none";
+        ProcessSizeMax = 0;
+      };
+    };
+  };
 }
