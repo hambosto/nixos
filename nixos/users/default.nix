edbe4e12d0b72533bcf4ccc707d2f91fbeba9002
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2026-07-18 13:01:38 +0700

    chore: remove unused networkmanager from users

--- nixos/users/default.nix
+++ nixos/users/default.nix
@@ -3,10 +3,7 @@
   users = {
     mutableUsers = false;
     users.ilham = {
-      extraGroups = [
-        "networkmanager"
-        "wheel"
-      ];
+      extraGroups = [ "wheel" ];
       hashedPassword = "$y$j9T$Lbwif0l9QnHjD7TG0Xekp/$zwD2gb/43zdE4nBSTibnjVg1fzulII7us1I2xB75QB6";
       isNormalUser = true;
       shell = pkgs.fish;
